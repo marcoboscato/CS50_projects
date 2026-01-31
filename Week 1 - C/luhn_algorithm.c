2edd95728459306fb8eb1782f06855410e33a774
Marco Boscato <boscatomarco@gmail.com>
2026-01-31 15:57:09 +0100

    add all week 1 projects

---
+++ Week 1 - C/luhn_algorithm.c
@@ -0,0 +1,177 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <cs50.h>
+
+
+/*
+    This is a small program that identifies the type of credit card based on its number.
+    The algorithm is based on Luhn's algorithm.
+*/
+
+
+void put_digits(long num, int num_digits[]);
+int calculate_productdigits(int num_digits[], int len);
+int check_validation(int n);
+void card_type(long user_number, int len_number);
+
+
+// --- main function ---
+int main(void)
+{
+    // get user input
+    string user_input = get_string("Insert your credit card number here: ");
+    //trasform into int32
+    long user_number = atol(user_input);
+
+    // get the lenght of the string input
+    int len_number = strlen(user_input);
+    printf("lenghth of the card numer: %i\n", len_number);
+
+    // initialize an array to store the digits
+    int num_digits[len_number];
+    put_digits(user_number, num_digits);
+
+    //check print: Print all credit card digits
+    printf("Credit card digits: (last to first)\n");
+    for (int i = 0; i < len_number; i++)
+    {
+        printf("%i ", num_digits[i]);
+    }
+    printf("\n");
+
+    // sum of the product terms
+    int prod_sum = calculate_productdigits(num_digits, len_number);
+
+    // add the other digits
+    int otherhalf_sum = 0;
+    printf("other remain digits: (last to first)\n");
+    for (int i=0; i<len_number; i+=2)
+    {
+        printf("%i ", num_digits[i]); //check print
+        otherhalf_sum += num_digits[i];
+    }
+    printf("\n");
+
+    // total sum
+    int total_sum = otherhalf_sum + prod_sum;
+
+    printf("Total sum: %i\n", total_sum);
+
+    //check validation
+    int validation = check_validation(total_sum);
+
+    //Give answer
+    if (validation != 1 || len_number>16)
+    {
+        printf("the card is not valid!\n");
+    }
+    else
+    {
+        card_type(user_number, len_number);
+    }
+
+
+}
+
+
+
+// --- utility functions
+void put_digits(long num, int num_digits[])
+{
+    // store the number digits inside the array (mirrored)
+    int c = 0;
+    while (num != 0)
+    {
+        num_digits[c] = num % 10;
+        num /= 10;
+        c++;
+    }
+}
+
+
+
+int calculate_productdigits(int num_digits[], int len)
+{
+    int prod_digits_array[len/2];
+
+    int j = 0;
+    for (int i = 1; i < len; i=i+2)
+    {
+        prod_digits_array[j] = 2*num_digits[i];
+        j++;
+    }
+    // check print
+    printf("Product of pair digits (second-to-last)\n");
+    for (int i = 0; i < len/2; i++)
+    {
+        printf("%i ", prod_digits_array[i]);
+    }
+    printf("\n");
+
+    //sum the product digits
+    int sum = 0;
+    for (int i = 0; i < len/2; i++)
+    {
+        if (prod_digits_array[i] >= 10)
+        {
+            int temp_a = prod_digits_array[i] % 10;
+            int temp_b = prod_digits_array[i] / 10;
+            sum = sum + temp_a + temp_b;
+        }else
+        {
+            sum = sum + prod_digits_array[i];
+        }
+    }
+    printf("Sum of the pair product digits: %i \n", sum);
+    return sum;
+}
+
+
+
+int check_validation(int n)
+{
+    if (n % 10 == 0)
+    {
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+
+
+void card_type(long user_number, int len_number)
+{
+    // Get first two digits
+    long temp = user_number;
+    while (temp >= 100)
+    {
+        temp /= 10;
+    }
+    int two_digits = temp;
+    printf("first credt card tow digits: %i\n", two_digits);
+
+    // print the results (is thare's any)
+    printf("the credit card is: ");
+    // American Express
+    if (len_number == 15 && (two_digits==34 || two_digits==37))
+    {
+        printf("American Express!\n");
+    }
+    // MasterCard
+    else if (len_number == 16 && two_digits>=51 && two_digits<=55)
+    {
+        printf("MasterCard!\n");
+    }
+    // Visa
+    else if ((two_digits/10)==4 && (len_number == 13 || len_number == 16))
+    {
+        printf("Visa!\n");
+    }
+    else
+    {
+        printf("not valid!\n");
+    }
+}
---
+++ Week 1 - C/mario_pyramid.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <cs50.h>
+
+/*
+Small script that prints a double half-pyramid of a specified height, based on the classic Super Mario Bros. game.
+The user is prompted to input the height of the pyramid, which must be between 1 and 8 inclusive.
+*/
+
+
+int choose_high(void);
+void print_block(int n);
+void print_space(int n);
+void make_pyramid(int high);
+
+
+// main
+int main(void)
+{
+    int high = choose_high();
+
+    make_pyramid(high);
+}
+
+
+
+// functions
+
+int choose_high(void)
+{
+    int high;
+
+    while(true)
+    {
+        high = get_int("How tall is the pyramid? [1-8] ");
+
+        if (high>=1 & high<=8)
+        {
+            break;
+        }
+        else
+        {
+            printf("Must be between 1 and 8!\n");
+            continue;
+        }
+    }
+
+    return high;
+}
+
+
+
+void print_block(int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        printf("#");
+    }
+}
+
+void print_space(int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        printf(" ");
+    }
+}
+
+
+void make_pyramid(int high)
+{
+    //initial space block
+    int s = high-1;
+    //initial # block
+    int b = 1;
+
+    for(int i=high; i>0; i--)
+    {
+        print_space(s);
+        print_block(b);
+        print_space(2);
+        print_block(b);
+        print_space(s);
+        printf("\n");
+        s--;
+        b++;
+    }
+}
