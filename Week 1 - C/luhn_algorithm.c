2abf0b55baf5164304dc86ef620c0a2fe4293440
Marco Boscato <boscatomarco@gmail.com>
2026-01-31 16:22:06 +0100

    fixed luhn

--- Week 1 - C/luhn_algorithm.c
+++ Week 1 - C/luhn_algorithm.c
@@ -1,27 +1,24 @@
+#include <cs50.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <cs50.h>
-
 
 /*
     This is a small program that identifies the type of credit card based on its number.
     The algorithm is based on Luhn's algorithm.
 */
 
-
 void put_digits(long num, int num_digits[]);
 int calculate_productdigits(int num_digits[], int len);
 int check_validation(int n);
 void card_type(long user_number, int len_number);
-
 
 // --- main function ---
 int main(void)
 {
     // get user input
     string user_input = get_string("Insert your credit card number here: ");
-    //trasform into int32
+    // trasform into int32
     long user_number = atol(user_input);
 
     // get the lenght of the string input
@@ -32,7 +29,7 @@ int main(void)
     int num_digits[len_number];
     put_digits(user_number, num_digits);
 
-    //check print: Print all credit card digits
+    // check print: Print all credit card digits
     printf("Credit card digits: (last to first)\n");
     for (int i = 0; i < len_number; i++)
     {
@@ -46,9 +43,9 @@ int main(void)
     // add the other digits
     int otherhalf_sum = 0;
     printf("other remain digits: (last to first)\n");
-    for (int i=0; i<len_number; i+=2)
+    for (int i = 0; i < len_number; i += 2)
     {
-        printf("%i ", num_digits[i]); //check print
+        printf("%i ", num_digits[i]); // check print
         otherhalf_sum += num_digits[i];
     }
     printf("\n");
@@ -58,23 +55,19 @@ int main(void)
 
     printf("Total sum: %i\n", total_sum);
 
-    //check validation
+    // check validation
     int validation = check_validation(total_sum);
 
-    //Give answer
-    if (validation != 1 || len_number>16)
+    // Give answer
+    if (validation != 1 || len_number > 16)
     {
-        printf("the card is not valid!\n");
+        printf("INVALID\n");
     }
     else
     {
         card_type(user_number, len_number);
     }
-
-
 }
-
-
 
 // --- utility functions
 void put_digits(long num, int num_digits[])
@@ -89,36 +82,35 @@ void put_digits(long num, int num_digits[])
     }
 }
 
-
-
 int calculate_productdigits(int num_digits[], int len)
 {
-    int prod_digits_array[len/2];
+    int prod_digits_array[len / 2];
 
     int j = 0;
-    for (int i = 1; i < len; i=i+2)
+    for (int i = 1; i < len; i = i + 2)
     {
-        prod_digits_array[j] = 2*num_digits[i];
+        prod_digits_array[j] = 2 * num_digits[i];
         j++;
     }
     // check print
     printf("Product of pair digits (second-to-last)\n");
-    for (int i = 0; i < len/2; i++)
+    for (int i = 0; i < len / 2; i++)
     {
         printf("%i ", prod_digits_array[i]);
     }
     printf("\n");
 
-    //sum the product digits
+    // sum the product digits
     int sum = 0;
-    for (int i = 0; i < len/2; i++)
+    for (int i = 0; i < len / 2; i++)
     {
         if (prod_digits_array[i] >= 10)
         {
             int temp_a = prod_digits_array[i] % 10;
             int temp_b = prod_digits_array[i] / 10;
             sum = sum + temp_a + temp_b;
-        }else
+        }
+        else
         {
             sum = sum + prod_digits_array[i];
         }
@@ -127,20 +119,17 @@ int calculate_productdigits(int num_digits[], int len)
     return sum;
 }
 
-
-
 int check_validation(int n)
 {
     if (n % 10 == 0)
     {
         return 1;
     }
-    else{
+    else
+    {
         return 0;
     }
 }
-
-
 
 void card_type(long user_number, int len_number)
 {
@@ -153,25 +142,23 @@ void card_type(long user_number, int len_number)
     int two_digits = temp;
     printf("first credt card tow digits: %i\n", two_digits);
 
-    // print the results (is thare's any)
-    printf("the credit card is: ");
     // American Express
-    if (len_number == 15 && (two_digits==34 || two_digits==37))
+    if (len_number == 15 && (two_digits == 34 || two_digits == 37))
     {
-        printf("American Express!\n");
+        printf("AMEX!\n");
     }
     // MasterCard
-    else if (len_number == 16 && two_digits>=51 && two_digits<=55)
+    else if (len_number == 16 && two_digits >= 51 && two_digits <= 55)
     {
-        printf("MasterCard!\n");
+        printf("MASTERCARD!\n");
     }
     // Visa
-    else if ((two_digits/10)==4 && (len_number == 13 || len_number == 16))
+    else if ((two_digits / 10) == 4 && (len_number == 13 || len_number == 16))
     {
-        printf("Visa!\n");
+        printf("VISA\n");
     }
     else
     {
-        printf("not valid!\n");
+        printf("INVALID\n");
     }
 }
